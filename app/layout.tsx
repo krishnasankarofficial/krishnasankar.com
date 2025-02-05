1d0dc45356ca8ca33f1e9b053ce0fc81eba296a5
Ashik <ashikps957@gmail.com>
2025-02-05 13:38:08 +0530

    Changes

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Work_Sans, Raleway } from "next/font/google";
+import "./globals.css";
+
+const workSans = Work_Sans({
+  variable: "--font-work-sans",
+  subsets: ["latin"],
+});
+
+const raleway = Raleway({
+  variable: "--font-raleway",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Krishnasankar | Freelance Web Developer",
+  description: "Krishnasankar is a freelance web developer specializing in modern web technologies and responsive design. Discover his portfolio and services.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${workSans.variable} ${raleway.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <h1>Krishnasankar | Freelance Web Developer</h1>
+      <p>
+        Krishnasankar is a freelance web developer specializing in modern web technologies and responsive design. 
+        Discover his portfolio and services.
+      </p>
+    </div>  
+  );
+}
