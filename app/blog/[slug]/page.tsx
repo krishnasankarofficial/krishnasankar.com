12acf4495e6097f291f2aed2f44c0a53125f3740
Krishnasankar <krishnasankarofficial@gmail.com>
2025-03-19 15:39:10 +0530

    Change

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogContentPage() {
+    return (
+        <div className="w-full min-h-screen flex flex-col items-center justify-center">
+        <h1>Blog</h1>
+        <p>This is the blog content.</p>
+        </div>
+    );
+}
